b31f43fbecb2c3565cbcd9eafa67d6eae05cc552
Sea-Snell <sea_snell@icloud.com>
2021-01-03 17:04:28 -0600

    bugs fixed for epa model

--- epa_model.py
+++ epa_model.py
@@ -19,14 +19,10 @@ class EPAModel(nn.Module):
         # x - (batch_size, player, dim)
         # attn_mask - (batch_size, player). 1 if should be masked, 0 else.
         hidden = self.encoder(x.permute(1, 0, 2).contiguous(), src_key_padding_mask=attn_mask==1)
-        return self.output(hidden)
+        return torch.sum(self.output(hidden).permute(1, 0, 2).squeeze() * (1 - attn_mask), dim=1) / torch.sum(1 - attn_mask, dim=1)
 
 team_idxs = {'home': 0, 'away': 1, 'football': 2, 'unk': 3}
 direction_idxs = {'left': 0, 'right': 1, 'unk': 2}
-position_idxs = {'CB': 0, 'DB': 1, 'DE': 2, 'DL': 3, 'FB': 4, 
-                'FS': 5, 'HB': 6, 'ILB': 7, 'LB': 8, 'MLB': 9, 
-                'NT': 10, 'OLB': 11, 'QB': 12, 'RB': 13, 'S': 14, 
-                'SS': 15, 'TE': 16, 'WR': 17, 'unk': 18}
 position_idxs = {'P': 0, 'FB': 1, 'DL': 2, 'FS': 3, 'CB': 4, 'TE': 5, 'DT': 6, 
                 'DB': 7, 'RB': 8, 'LS': 9, 'MLB': 10, 'SS': 11, 'OLB': 12, 'QB': 13, 
                 'DE': 14, 'ILB': 15, 'S': 16, 'LB': 17, 'NT': 18, 'HB': 19, 'K': 20, 'WR': 21, 
@@ -54,6 +50,7 @@ class Dataset(torch.utils.data.Dataset):
         # dim = 3, first two corridinates are x and y, the last is team label
         week, play, frame = self.combined_frames[key]
         raw_data = self.data_frames[week].query('playId=={play} & frameId=={frame}'.format(play=play, frame=frame))[['x', 'y', 'team', 's', 'o', 'a', 'playDirection', 'position', 'epa']].values.tolist()
+        
         x, y, team, s, o, a, dir, pos, epa = list(zip(*raw_data))
         xs = np.array([item if not np.isnan(item) else 0.0 for item in x])
         ys = np.array([item if not np.isnan(item) else 0.0 for item in y])
@@ -75,6 +72,7 @@ def pad_batch(data_batch_x, data_batch_y):
     batch_padded_y = []
     for batch_item in data_batch_x:
         pad1 = torch.zeros((batch_item.shape[0], max_players-batch_item.shape[1], batch_item.shape[2]))
+        pad1[:, :, 5] = team_idxs['unk']
         pad1[:, :, 6] = direction_idxs['unk']
         pad1[:, :, 7] = position_idxs['unk']
         batch_padded_x.append(torch.cat([batch_item, pad1], dim=1))
@@ -90,29 +88,29 @@ if __name__ == "__main__":
 
     wandb.init(project="nfl-big-data-bowl-2021-epa-model")
 
-    train_dataset = Dataset(['data/epa-included-week-%d.csv' % (i) for i in range(1, 2)])
-    val_dataset = Dataset(['data/epa-included-week-%d.csv' % (i) for i in range(2, 3)])
+    train_dataset = Dataset(['data/epa-included-week-%d.csv' % (i) for i in range(1, 16)])
+    val_dataset = Dataset(['data/epa-included-week-%d.csv' % (i) for i in range(16, 18)])
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=True)
 
-    model = EPAModel(64, 4, 256, 6).float()
+    model = EPAModel(69, 3, 256, 2).float()
     #add embeddings for team, ball label to the model
-    model.team_embeddings = nn.Embedding(len(team_idxs), 29)
-    model.dir_embeddings = nn.Embedding(len(direction_idxs), 29)
-    model.pos_embeddings = nn.Embedding(len(position_idxs), 30)
+    model.team_embeddings = nn.Embedding(len(team_idxs), 32)
+    model.dir_embeddings = nn.Embedding(len(direction_idxs), 32)
+    model.pos_embeddings = nn.Embedding(len(position_idxs), 32)
 
     model = model.to(device)
     wandb.watch(model)
     model.train()
     config = wandb.config
     config.epochs = 10
-    config.bsize = 8
+    config.bsize = 32
     config.val_steps = 16
 
-    optim = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
-    lr_schedule = transformers.get_polynomial_decay_schedule_with_warmup(optim, num_warmup_steps=2000,
-                                                                         num_training_steps=(config.epochs*(len(train_dataset)//(config.bsize))),
-                                                                         power=1.0, lr_end=0.0)
+    optim = torch.optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01)
+    # lr_schedule = transformers.get_polynomial_decay_schedule_with_warmup(optim, num_warmup_steps=2000,
+    #                                                                      num_training_steps=(config.epochs*(len(train_dataset)//(config.bsize))),
+    #                                                                      power=1.0, lr_end=0.0)
 
     step = 0
     best_val_loss = float('inf')
@@ -125,24 +123,24 @@ if __name__ == "__main__":
             curr_batch_y.append(item_y)
             if len(curr_batch_x) < config.bsize:
                 continue
-
             #pad batch
             items_x, items_y = pad_batch(curr_batch_x, curr_batch_y)
             # first two items in input data are x, y the third item is a scalar representing team, get the embedding for this scalar and concat with x and y
             items_x = items_x.to(device)
-            truth = items_y.to(device)
+            truth = items_y.to(device)[:,0]
             input_data = torch.cat([items_x[:, :, :5], model.team_embeddings(items_x[:, :, 5].long()) + model.dir_embeddings(items_x[:, :, 6].long()), model.pos_embeddings(items_x[:, :, 7].long())], dim=-1).float().contiguous()
             attn_mask = (items_x[:, :, 5] == team_idxs['unk']).float()
             # outputs 4 things for each time step and each player:
             # the first 2 are the predicted mean x any y and the last 2 are their standard deviations
             output = model(input_data, attn_mask)
             # MLE loss function that takes into account variance prediction, can be negative (assumes independent gaussians, this is a strong assumption, but outputing a whole covariance matrix would be hard)
-            loss = torch.mean((output - truth)**2 * (1 - attn_mask))
+            loss = torch.mean((output - truth)**2)
             optim.zero_grad()
             loss.backward()
             optim.step()
-            lr_schedule.step()
-            curr_batch = []
+            # lr_schedule.step()
+            curr_batch_x = []
+            curr_batch_y = []
 
             if step % 100 == 0:
                 # eval model
@@ -159,13 +157,14 @@ if __name__ == "__main__":
                     val_items_x, val_items_y = pad_batch(val_batch_x, val_batch_y)
 
                     val_items_x = val_items_x.to(device)
-                    val_truth = val_items_y.to(device)
+                    val_truth = val_items_y.to(device)[:,0]
                     val_input_data = torch.cat([val_items_x[:, :, :5], model.team_embeddings(val_items_x[:, :, 5].long()) + model.dir_embeddings(val_items_x[:, :, 6].long()), model.pos_embeddings(val_items_x[:, :, 7].long())], dim=-1).float().contiguous()
                     val_attn_mask = (val_items_x[:, :, 5] == team_idxs['unk']).float()
                     val_output = model(val_input_data, val_attn_mask)
-                    val_loss = torch.mean((val_output - val_truth)**2 * (1 - val_attn_mask))
+                    val_loss = torch.mean((val_output - val_truth)**2)
                     total_val_loss += val_loss
-                    val_batch = []
+                    val_batch_x = []
+                    val_batch_y = []
                     val_step += 1
                     if val_step >= config.val_steps:
                         break
